a84305a49591f6d7339ffd71bed216b01670f8a0
chriskao <chris18@stanford.edu>
2014-02-25 00:59:41 -0800

    Delete test.sql

--- dbfile/test.sql
+++
@@ -1,2 +0,0 @@
-select Drinks.drinkName as drinkName from Drinks, Ingredients, Recipe 
-where Drinks.drinkID = Recipe.drinkID and Ingredients.ingredientID = Recipe.ingredientID and Ingredients.ingredientID =1 and Drinks.ingredientNumber =2;
